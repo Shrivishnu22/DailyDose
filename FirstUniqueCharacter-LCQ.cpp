cb4172e18f348e7da87e2080433feb864f1fa553
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-02-24 06:36:05 +0530

    Initial Commit

---
+++ FirstUniqueCharacter-LCQ.cpp
@@ -0,0 +1,57 @@
+/*
+First Unique Character in a String
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+*/
+
+#include<stdio.h>
+#include<bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	unordered_map <int, int> m;
+	string s;
+	cin >> s;
+	char element;
+	int flag = true;
+	for (int i = 0; i < s.size(); i++) {
+		m[s[i]]++;
+	}
+	for (auto &it : m) {
+		if (it.second == 1) {
+			element = (char)it.first;
+			break;
+		}
+	}
+	for (int k = 0; k < s.size(); k++) {
+		if (m[s[k]] == 1 ) {
+			cout << k << endl;
+			flag = false;
+			break;
+		}
+	}
+	if (flag) cout << -1 << endl;
+
+}

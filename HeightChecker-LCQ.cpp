5f58ac199eebca36c53fc61432388f59b5a8d683
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-02-27 06:48:02 +0530

    Add O(N) Approach

--- HeightChecker-LCQ.cpp
+++ HeightChecker-LCQ.cpp
@@ -71,3 +71,20 @@ int main() {
 	cout << cnt << endl;
 
 }
+
+//O(N) Approach
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        auto copy = heights;
+        sort(copy.begin(), copy.end());
+        int count = 0;
+        for(int i = 0; i < heights.size(); i++)
+        {
+            if(heights[i] != copy[i])
+                count++;
+        }
+        return count;
+    }
+};

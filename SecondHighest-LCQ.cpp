033fd3d75de460cf13fef52cd572406b337b8fc5
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-05-24 07:11:14 +0530

    Create SecondHighest-LCQ.cpp

---
+++ SecondHighest-LCQ.cpp
@@ -0,0 +1,44 @@
+/*
+ Second Largest Digit in a String
+
+Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
+
+An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+ 
+
+Example 1:
+
+Input: s = "dfa12321afd"
+Output: 2
+Explanation: The digits that appear in s are [1, 2, 3]. The second largest digit is 2.
+
+Example 2:
+
+Input: s = "abc1111"
+Output: -1
+Explanation: The digits that appear in s are [1]. There is no second largest digit. 
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of only lowercase English letters and/or digits.
+*/
+
+class Solution {
+public:
+    int secondHighest(string s) {
+        int secondmax=-1,firstmax=-1;
+        for(char c:s){
+            if(c>='0' && c<='9')
+                if(c>firstmax){
+                    secondmax=firstmax;
+                    firstmax=c;
+                }
+            else if(c>secondmax && c<firstmax)
+                secondmax=c;
+        }
+        return secondmax==-1? -1 : secondmax-'0';
+    }
+};

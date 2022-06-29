1e6708cdc4c75f1975ec62a1b9206fdb2fc55bbd
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-06-30 05:11:00 +0530

    Create LargestOddNumber-LCQ

---
+++ LargestOddNumber-LCQ.cpp
@@ -0,0 +1,49 @@
+/*
+Largest Odd Number in String
+
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+
+
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+
+Example 2:
+
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+
+Example 3:
+
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+
+
+Constraints:
+
+1 <= num.length <= 105
+num only consists of digits and does not contain any leading zeros.
+*/
+
+class Solution {
+public:
+	string largestOddNumber(string num) {
+		string dummy = "";
+		for (int itr = num.size() - 1; itr >= 0; itr--) {
+			if ((num[itr] - '0') % 2 != 0)
+				return num.substr(0, itr + 1);
+		}
+		return dummy;
+	}
+};
+
+
+//substr() is used to access the range of string
+//substr(initial,final-1)

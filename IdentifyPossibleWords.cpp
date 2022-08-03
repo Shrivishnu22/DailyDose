44c66464643906923aa88f8ec7f7d3244a543185
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-08-03 14:11:16 +0530

    Update Question Description

--- IdentifyPossibleWords.cpp
+++ IdentifyPossibleWords.cpp
@@ -1,3 +1,17 @@
+/*
+The function is expected to find all the possible words from input2 that can replace the incomplete word input1, and 
+return the result in the format suggested below.
+Example1
+input1="Fi er"
+input2 = "Fever.filer:Filter: Fixer:fiber:fibre:tailor:offer" output string should be returned as "FILER:FIXER:FIBER"
+Note that
+•The output string should contain the set of all possible words that can replace the incomplete word in input1
+• all words in the output string should be stored in UPPER-CASE
+• all words in the output string should appear in the order in which they appeared in input2 ie in the above example we have FILER followed by FIXER followed by FIBER
+• While searching for input1 in input2 the case of the letters are ignored Le Fer matches with "filer as well as "Fixer" as well as fiber
+• IMPORTANT: If none of the words in input2 are possible candidates to replace input the output string should contain the string ERROR-009
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 

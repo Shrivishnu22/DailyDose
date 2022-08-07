53f1d026a08f1019dd1ddd284fef369ff76b88af
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-08-07 13:08:16 +0530

    Update UnlockingKey.cpp

--- UnlockingKey.cpp
+++ UnlockingKey.cpp
@@ -1,3 +1,23 @@
+/*
+Mr. Jason has captured your friend and has a collar around his neck. He has locked the collar with a given “locking key". Now it can only be opened with an 
+“unlocking key”. Your friend sees the locking key but he does not know how to find unlocking key.You can calculate the unlocking key if 
+you have the locking key, because the unlocking key will be the smallest (in magnitude) permutation of the digits of the locking key and will never start with zero. 
+Help your friend write an algorithm that outputs the unlocking key by taking key as an input.
+
+The input to the function/method consists of an argument lockingkey, an integer representing the 
+locking key.
+
+Output Format:
+Return an integer representing the unlocking key
+
+Constraints:
+-10^7 <= lockingkeys <= 10^7
+
+Sample Input : 62154
+Sample Output : 12456
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()

a7298ceb90a638a0d0e56c62caf63eaf9522cbb4
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-08-11 21:58:05 +0530

    Update KeyPad.cpp

--- KeyPad.cpp
+++ KeyPad.cpp
@@ -1,3 +1,20 @@
+/*
+Get a String from user, and to print corresponding number in a keypad format
+
+Input Format: Get a String
+Output Format: Print Corresponding Number Constraints:
+
+Sample Input 1:
+Welcome to Sixphrase team
+Sample Output 1:
+9 33 555 222 666 6 33 0 8 666 0 7777 444 99 7 44 777 2 7777 33 0 8 33 2 6
+
+Sample Input 2: Hai
+Sample Output 2:
+44 2 444
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()

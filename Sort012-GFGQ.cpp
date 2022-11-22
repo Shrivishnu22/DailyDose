30bedb65432148f7454319ff228565b98fd9891a
Shri Vishnu <62836567+Shrivishnu22@users.noreply.github.com>
2022-11-22 06:46:50 +0530

    Create Sort012-GFGQ.cpp

---
+++ Sort012-GFGQ.cpp
@@ -0,0 +1,100 @@
+/*
+Sort an array of 0s, 1s and 2s
+
+This problem is part of GFG SDE Sheet. Click here to view more.  
+
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Example 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 <= N <= 10^6
+0 <= A[i] <= 2
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        int zcnt=0,ocnt=0,tcnt=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0) zcnt++;
+            else if(a[i]==1)    ocnt++;
+            else    tcnt++;
+        }
+        int idx=0;
+        while(zcnt--)    a[idx++]=0;
+        while(ocnt--)   a[idx++]=1;
+        while(tcnt--)   a[idx++]=2;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
